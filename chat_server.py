12f0aebcb78df9eff710775e28061e71e62cebc1
zw1800 <83383832+zw1800@users.noreply.github.com>
2021-05-03 14:53:49 +0800

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -3,7 +3,7 @@ Created on Tue Jul 22 00:47:05 2014
 
 @author: alina, zzhang
 """
-
+import Game
 import time
 import socket
 import select
@@ -225,6 +225,7 @@ class Server:
                     for g in the_guys:
                         to_sock = self.logged_name2sock[g]
                         mysend(to_sock, json.dumps({"results": "start game!"}))
+                        Game.main()
                     self.number_of_accept = 0
                 else:
                     mysend(from_sock, json.dumps({"results": "waiting for others to respond"}))
